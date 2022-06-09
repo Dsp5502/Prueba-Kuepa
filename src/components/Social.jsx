eb230b3bea7cdbcceb7874d6dd8847f5821460bb
David Puerto <dsp5502@gmail.com>
2022-06-08 21:54:34 -0500

    logo social

--- src/components/Social.jsx
+++ src/components/Social.jsx
@@ -1,19 +1,37 @@
+import {
+  faFacebookSquare,
+  faWhatsappSquare,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { getRandomInt } from '../helpers';
 
 const Social = ({ clients }) => {
-  console.log(clients);
   const number = getRandomInt(0, clients.length);
   const clientsSocial = clients.slice(number - 6, number);
   return (
-    <div className=' h-screen  flex flex-col justify-between items-center hover:cursor-pointer'>
+    <div className='   flex flex-col justify-between items-center hover:cursor-pointer'>
       {clientsSocial.map((client) => (
         <div key={client.account_id} className=' w-20 h-20 rounded-lg my-2'>
-          <h2 className='text-xs text-center p-1 bg-slate-400 rounded-tr-lg rounded-tl-lg text-white '>
+          {client.plus ? (
+            <FontAwesomeIcon
+              icon={faFacebookSquare}
+              className='-mb-4 text-blue-500'
+            />
+          ) : (
+            <FontAwesomeIcon
+              icon={faWhatsappSquare}
+              className='-mb-4 text-green-500'
+            />
+          )}
+
+          <h2 className='textSmall text-center p-1 bg-slate-400 rounded-tr-lg rounded-tl-lg text-white '>
             {client.plus ? 'FACEBOOK' : 'WHATSAPP'}
           </h2>
-          <div className='h-14 bg-ebony-clay-700 rounded-br-lg rounded-bl-lg'>
-            <p className='text-white w-full text-center'>Jorge Garcia</p>
+          <div className='h-14 bg-ebony-clay-700 rounded-br-lg rounded-bl-lg flex justify-center items-center'>
+            <p className='text-white w-full text-center text-sm px-2'>
+              {client.name}
+            </p>
           </div>
         </div>
       ))}
